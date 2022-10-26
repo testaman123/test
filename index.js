c8b5dc41f072aa70f8f7a66cfa5a06c69b58bc7b
Aman <aman.singh@schbang.com>
2022-10-26 10:27:05 +0530

    Added .env

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 var express = require("express");
 var app = express();
 app.use(express.json());
+require("dotenv").config();
+
+const PORT = process.env.PORT;
 
 app.post("/", function (req, res) {
   res.send(req.body);
 });
 
-var server = app.listen(8081, function () {
+var server = app.listen(PORT, function () {
   var host = "localhost";
-  var port = "8081";
 
-  console.log("Example app listening at http://%s:%s", host, port);
+  console.log("Example app listening at http://%s:%s", host, PORT);
 });
